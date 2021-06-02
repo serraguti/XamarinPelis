1243d9ed728e4a19bd1e7b68d27b800bb3208c98
serraguti <pacoserranox@gmail.com>
2021-06-02 11:35:01 +0200

    comitts

---
+++ XamarinPelis/XamarinPelis/Models/Pelicula.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinPelis.Models
+{
+    public class Pelicula
+    {
+        [JsonProperty("idPelicula")]
+        public int IdPelicula { get; set; }
+        [JsonProperty("titulo")]
+        public String Titulo { get; set; }
+        [JsonProperty("argumento")]
+        public String Argumento { get; set; }
+        [JsonProperty("poster")]
+        public String Poster { get; set; }
+    }
+}
